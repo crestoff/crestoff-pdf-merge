17068f0dc6e1fa1eb7b5ec39febdd8d722b8c615
Dmitry Crestoff <crestoff@gmail.com>
2024-10-24 22:50:20 +0700

    Update FooterConfig.php

--- src/Config/FooterConfig.php
+++ src/Config/FooterConfig.php
@@ -4,16 +4,20 @@ namespace Karriere\PdfMerge\Config;
 
 class FooterConfig
 {
+    private $textColor;
+    private $lineColor;
+    private $margin;
+
     /**
      * @param ?RGB $textColor RGB array color for text
      * @param ?RGB $lineColor RGB array color for line
      * @param int $margin minimum distance (in "user units") between footer and bottom page margin
      */
-    public function __construct(
-        private $textColor = null,
-        private $lineColor = null,
-        private $margin = 0,
-    ) {
+    public function __construct($textColor = null, $lineColor = null, $margin = 0)
+    {
+        $this->textColor = $textColor;
+        $this->lineColor = $lineColor;
+        $this->margin = $margin;
     }
 
     public function textColor(): RGB
